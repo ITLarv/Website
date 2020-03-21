96fef354b80ac71c7f8980f0715f7cd60ead37e5
Peter Panduro <peter@kodifiera.se>
2020-03-21 14:57:14 +0100

    added drop-downs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 import '../css/navbar.css';
 
 
 export default function Navbar() {
+
+  const history = useHistory();
+
+  const btnClick = (e, l) => {
+    history.push(l);
+  }
+
   return (
-    <div className='navbar'>
-        
-        <nav>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/fair'>Fair</Link></li>
-                <li><Link to='/prelarv'>Prelarv</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-                <li><Link to='/student'>Students</Link></li>
-                <li><Link to='/company'>Conpanies</Link></li>
-            </ul>
-        </nav>
+    <div className='topnav'>
+      <div className="dropdown">
+        <button className="dropbtn" onClick={e => btnClick(e, '/')}>Home</button>
+        <div className="dropdown-content">
+          <Link to="/news">News</Link>
+          <Link to="/about">About</Link>
+          <Link to="/calendar">Calendar</Link>
+        </div>
+      </div>
+      <div className="dropdown">
+        <button className="dropbtn" onClick={e => btnClick(e, '/fair')}>Fair</button>
+        <div className="dropdown-content">
+          <Link to="/catalog">Catalog</Link>
+          <Link to="/map">Map</Link>
+        </div>
+      </div>
+      <Link to='/prelarv'>Prelarv</Link>
+      <Link to='/contact'>Contact</Link>
+      <div className="dropdown">
+        <button className="dropbtn" onClick={e => btnClick(e, '/student')}>Students</button>
+        <div className="dropdown-content">
+          <Link to="/students/faq">FAQ</Link>
+          <Link to="/hosts">Hosts</Link>
+          <Link to="/students/im">Individual meetings</Link>
+          <Link to="/students/calendar">Calendar</Link>
+        </div>
+      </div>
+      <div className="dropdown">
+        <button className="dropbtn" onClick={e => btnClick(e, '/company')}>Companies</button>
+        <div className="dropdown-content">
+          <Link to="/companies/faq">FAQ</Link>
+          <Link to="/companies/im">Individual meetings</Link>
+          <Link to="/companies/calendar">Calendar</Link>
+        </div>
+      </div>
     </div>
   );
 };
