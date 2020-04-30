0b5d81559b2c1ac71759f6985f774762a8bd95f8
Peter Panduro <peter@kodifiera.se>
2020-04-30 13:52:54 +0200

    Map isn't fricked up

--- src/pages/TestMap.tsx
+++ src/pages/TestMap.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-import "../../node_modules/leaflet/dist/leaflet.css";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import shadowIcon from "../img/leaflet-shadow.png";
+import icon from "../img/leaflet-icon.png";
 
 type State = {
 	lat: number;
@@ -8,22 +11,46 @@ type State = {
 	zoom: number;
 };
 
+export const customIcon = new L.Icon({
+	iconUrl: icon,
+	// iconRetinaUrl: "../",
+	iconAnchor: [25, 50],
+	popupAnchor: [0, -50],
+	iconSize: [50, 50],
+	shadowUrl: shadowIcon,
+	shadowSize: [68, 95],
+	shadowAnchor: [20, 92],
+});
+
 class TestMap extends Component<{}, State> {
 	state = {
 		lat: 65.6179119971132,
 		lng: 22.141948071363544,
-		zoom: 13,
+		zoom: 15,
+	};
+
+	change = (e) => {
+		console.log({ e });
+	};
+
+	click = (e) => {
+		console.log({ e });
 	};
 
 	render() {
 		const position = [this.state.lat, this.state.lng];
 		return (
-			<Map center={position} zoom={this.state.zoom}>
+			<Map
+				center={position}
+				zoom={this.state.zoom}
+				onViewportChanged={this.change}
+				onClick={this.click}
+			>
 				<TileLayer
 					attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
-				<Marker position={position}>
+				<Marker position={position} icon={customIcon}>
 					<Popup>
 						A pretty CSS3 popup. <br /> Easily customizable.
 					</Popup>
