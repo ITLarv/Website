2ed917c12533718b268907e7b7ddb01c3574656b
Peter Larsson <peter@kodifiera.se>
2020-09-16 10:29:29 +0200

    Initial application once again points to jexpo. Needs CSS though

--- src/pages/InitialApplication.jsx
+++ src/pages/InitialApplication.jsx
@@ -3,16 +3,15 @@ import JexpoInitialApplication from "../components/Jexpo/JexpoInitialApplication
 
 export default function InitialAplication() {
 	return (
-		<>
-			<div className="iframe-container">
-				<iframe
-					title="test"
-					className="form"
-					src="https://docs.google.com/forms/d/e/1FAIpQLScSqN4U9W4HmGEitNmS06w-3ePJfmM3VdRVWTz8q76LPtQxhA/viewform?embedded=true"
-				>
-					Läser in …
-				</iframe>
-			</div>
-		</>
+		<div className="iframe-container">
+			<iframe
+				title="jexpo-initial-application"
+				className="iframe-jexpo"
+				srcDoc={JexpoInitialApplication}
+				// src="https://docs.google.com/forms/d/e/1FAIpQLScSqN4U9W4HmGEitNmS06w-3ePJfmM3VdRVWTz8q76LPtQxhA/viewform?embedded=true"
+			>
+				Läser in …
+			</iframe>
+		</div>
 	);
 }
