7157668c6cc04c346717579a2364604b760be8b6
Peter Larsson <peter@kodifiera.se>
2020-10-01 11:26:52 +0200

    Removed music quiz stuff

--- src/App.jsx
+++ src/App.jsx
@@ -136,18 +136,6 @@ function Page() {
 							path="/organizations/inbjudan_innovationsrum.pdf"
 							component={InvitationInnovation}
 						/>
-						<Route exact path="/musicquiz1">
-							<GoogleForm
-								height="850px"
-								formUrl="https://docs.google.com/forms/d/e/1FAIpQLSdofQpERd9kmjVS2XrKLYs-Fyqw9U7xM0i762HXsEt8WCWt2Q/viewform?embedded=true"
-							/>
-						</Route>
-						<Route exact path="/musicquiz2">
-							<GoogleForm
-								height="850px"
-								formUrl="https://docs.google.com/forms/d/e/1FAIpQLSfA34Gltez6wZP0DnUtU-2HTek5LtAwXiCN6yovMZJnfaWIIg/viewform?embedded=true"
-							/>
-						</Route>
 						<Redirect from="/postbeskrivningar/pg" to="/involved" />
 						<Redirect from="/foretag/inledande-anmalan/" to="/organizations" />
 						<Route component={NotFound} />
