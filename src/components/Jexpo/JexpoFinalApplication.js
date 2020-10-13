203d040dbbf003608f7bc9459bc2daa613ffaeea
Tovah Parnes <tovah@Tovahs-MacBook-Pro.local>
2020-10-13 12:51:14 +0200

    fixed small error with FA

--- src/components/Jexpo/JexpoFinalApplication.js
+++ src/components/Jexpo/JexpoFinalApplication.js
@@ -3,7 +3,7 @@ const JexpoFinalApplication = `
 	<div id="app-dialog" class="jexpo jexpo-forms"></div>
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.7/es5-shim.min.js"></script>
-	<script src="http://cdnjs.cloudflare.com/ajax/libs/dom4/1.8.3/dom4.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/dom4/1.8.3/dom4.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.1/es6-shim.min.js"></script>
 
 	<script type="text/javascript" src="https://storage.googleapis.com/jexpo-cdn/vendor-v18.min.js"></script>
