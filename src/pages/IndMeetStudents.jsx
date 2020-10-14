abdcf57085aa0e0e3a03eb71777d4a54e69b659f
Tovah Parnes <tovah@Tovahs-MacBook-Pro.local>
2020-10-14 14:34:55 +0200

    Uppdated IM text under Students and added a google form (not imbedded because it didn't work with upploading files)

--- src/pages/IndMeetStudents.jsx
+++ src/pages/IndMeetStudents.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 export default function InvidualMeetings() {
@@ -9,6 +10,11 @@ export default function InvidualMeetings() {
 			<p>{t("IM.STUDENTS.INFO_1")}</p>
 			<p>{t("IM.STUDENTS.INFO_2")}</p>
 			<p>{t("IM.STUDENTS.INFO_3")}</p>
+			<p>{t("IM.STUDENTS.INFO_4")}</p>
+
+			<a className="button" href="https://forms.gle/EJMax49CLeboeWHa6">
+				{t("IM.STUDENTS.APPLY")}
+			</a>
 		</>
 	);
 }
