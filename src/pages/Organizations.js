183e3de602cefee5fec2b5be5daf5625903b4b84
Tovah Parnes <tovah@Tovahs-MacBook-Pro.local>
2020-10-20 21:04:31 +0200

    VCF guide working now

--- src/pages/Organizations.js
+++ src/pages/Organizations.js
@@ -36,10 +36,7 @@ export default function Companies() {
 				<NavLink className="button" to="/organizations/prislista_digital.pdf">
 					{t("ORGANIZATIONS.THE_FAIR.PORTFOLIO.HEADER")}
 				</NavLink>
-				<NavLink
-					className="button"
-					to="/organizations/organizations/company_VCF_guide.pdf"
-				>
+				<NavLink className="button" to="/organizations/company_VCF_guide.pdf">
 					{t("ORGANIZATIONS.THE_FAIR.PRICING.BUTTON")}
 				</NavLink>
 			</div>
