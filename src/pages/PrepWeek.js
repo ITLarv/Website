cdde83f5bf3556318afc976c9eaf30536546fd23
Tovah Parnes <tovah@Tovahs-MacBook-Pro.local>
2020-10-31 18:43:20 +0100

    updated prepweek info

--- src/pages/PrepWeek.js
+++ src/pages/PrepWeek.js
@@ -12,34 +12,86 @@ export default function Prelarv() {
 			<p>{t("PREP_WEEK.INFO")}</p>
 
 			<h1>{t("PREP_WEEK.ACTIVITIES.HEADER")}</h1>
-			<p>{t("PREP_WEEK.ACTIVITIES.INFO")}</p>
-			<ul>
-				<li>{t("PREP_WEEK.ACTIVITIES.LIST_1")}</li>
-				<li>{t("PREP_WEEK.ACTIVITIES.LIST_2")}</li>
-				<li>{t("PREP_WEEK.ACTIVITIES.LIST_3")}</li>
 
-				<li className="indentList">{t("PREP_WEEK.ACTIVITIES.LIST_3_1")}</li>
-				<li className="indentList">{t("PREP_WEEK.ACTIVITIES.LIST_3_2")}</li>
-				<li className="indentList">{t("PREP_WEEK.ACTIVITIES.LIST_3_3")}</li>
-
-				<li>{t("PREP_WEEK.ACTIVITIES.LIST_4")}</li>
-				<li>{t("PREP_WEEK.ACTIVITIES.LIST_5")}</li>
-				<li>{t("PREP_WEEK.ACTIVITIES.LIST_6")}</li>
-				<li>{t("PREP_WEEK.ACTIVITIES.LIST_7")}</li>
-			</ul>
-
-			<p>{t("PREP_WEEK.ACTIVITIES.MORE")}</p>
-
+			<h2>{t("PREP_WEEK.ACTIVITIES.EVENT_2.HEADER")}</h2>
+			<h3>{t("PREP_WEEK.ACTIVITIES.EVENT_2.LOC_TIME")}</h3>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_2.INFO_1")}</p>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_2.INFO_2")}</p>
 			<div className="ButtonDiv">
-				{showCalendar && (
-					<NavLink className="button" to="/home/calendar">
-						{t("GENERAL.CALENDAR")}
-					</NavLink>
-				)}
-				<NavLink className="button" to="/organizations">
-					{t("GENERAL.ORGANIZATIONS")}
-				</NavLink>
+				<a className="button" href="https://fb.me/e/5WudSXeDy" target="_blank">
+					{t("PREP_WEEK.ACTIVITIES.LINK_TEXT")}
+				</a>
 			</div>
+
+			<h2>{t("PREP_WEEK.ACTIVITIES.EVENT_3.HEADER")}</h2>
+			<h3>{t("PREP_WEEK.ACTIVITIES.EVENT_3.LOC_TIME")}</h3>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_3.INFO_1")}</p>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_3.INFO_2")}</p>
+			<div className="ButtonDiv">
+				<a className="button" href="https://fb.me/e/33Rw5IJXe" target="_blank">
+					{t("PREP_WEEK.ACTIVITIES.LINK_TEXT")}
+				</a>
+			</div>
+
+			<h2>{t("PREP_WEEK.ACTIVITIES.EVENT_4.HEADER")}</h2>
+			<h3>{t("PREP_WEEK.ACTIVITIES.EVENT_4.LOC_TIME")}</h3>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_4.INFO_1")}</p>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_4.INFO_2")}</p>
+			<div className="ButtonDiv">
+				<a className="button" href="https://fb.me/e/3wELGiJUS" target="_blank">
+					{t("PREP_WEEK.ACTIVITIES.LINK_TEXT")}
+				</a>
+			</div>
+
+			<h2>{t("PREP_WEEK.ACTIVITIES.EVENT_5.HEADER")}</h2>
+			<h3>{t("PREP_WEEK.ACTIVITIES.EVENT_5.LOC_TIME")}</h3>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_5.INFO_1")}</p>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_5.INFO_2")}</p>
+			<div className="ButtonDiv">
+				<a className="button" href="https://fb.me/e/fo1cO29jS" target="_blank">
+					{t("PREP_WEEK.ACTIVITIES.LINK_TEXT")}
+				</a>
+			</div>
+
+			<h2>{t("PREP_WEEK.ACTIVITIES.EVENT_6.HEADER")}</h2>
+			<h3>{t("PREP_WEEK.ACTIVITIES.EVENT_6.LOC_TIME")}</h3>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_6.INFO_1")}</p>
+			<div className="ButtonDiv">
+				<a className="button" href="https://fb.me/e/3phvbL3no" target="_blank">
+					{t("PREP_WEEK.ACTIVITIES.LINK_TEXT")}
+				</a>
+			</div>
+
+			<h2>{t("PREP_WEEK.ACTIVITIES.EVENT_7.HEADER")}</h2>
+			<h3>{t("PREP_WEEK.ACTIVITIES.EVENT_7.LOC_TIME")}</h3>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_7.INFO_1")}</p>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_7.INFO_2")}</p>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_7.INFO_3")}</p>
+			<p>{t("PREP_WEEK.ACTIVITIES.EVENT_7.INFO_4")}</p>
+			<div className="ButtonDiv">
+				<a className="button" href="https://fb.me/e/1MGMdpguc" target="_blank">
+					{t("PREP_WEEK.ACTIVITIES.LINK_TEXT")}
+				</a>
+			</div>
+
+			<h1>{t("PREP_WEEK.CONTESTS.HEADER")}</h1>
+
+			<h2>{t("PREP_WEEK.CONTESTS.CONTEST_1.HEADER")}</h2>
+			<p>{t("PREP_WEEK.CONTESTS.CONTEST_1.INFO_1")}</p>
+			<p>{t("PREP_WEEK.CONTESTS.CONTEST_1.INFO_2")}</p>
+			<p>{t("PREP_WEEK.CONTESTS.CONTEST_1.INFO_3")}</p>
+			<p>{t("PREP_WEEK.CONTESTS.CONTEST_1.INFO_4")}</p>
+
+			<h2>{t("PREP_WEEK.CONTESTS.CONTEST_2.HEADER")}</h2>
+			<p>{t("PREP_WEEK.CONTESTS.CONTEST_2.INFO_1")}</p>
+
+			<h2>{t("PREP_WEEK.CONTESTS.CONTEST_3.HEADER")}</h2>
+			<p>{t("PREP_WEEK.CONTESTS.CONTEST_3.INFO_1")}</p>
+			<ul>
+				<li>{t("PREP_WEEK.CONTESTS.CONTEST_3.LI_1")}</li>
+				<li>{t("PREP_WEEK.CONTESTS.CONTEST_3.LI_2")}</li>
+				<li>{t("PREP_WEEK.CONTESTS.CONTEST_3.LI_3")}</li>
+			</ul>
 		</div>
 	);
 }
