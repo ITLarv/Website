ed896463f2a28bf1781ce1c7b94812f00693cec1
TovahParnes <tovah@parnes.com>
2020-12-30 17:15:35 +0100

    updated settings

--- src/pages/Students.js
+++ src/pages/Students.js
@@ -28,13 +28,15 @@ export default function Students() {
 				</NavLink>
 			</div>
 
-			<h1>{t("GENERAL.GET_INVOLVED")}</h1>
-			<p>{t("STUDENTS.INVOLVED")}</p>
+			<div classname="involved">
+				<h1>{t("GENERAL.GET_INVOLVED")}</h1>
+				<p>{t("STUDENTS.INVOLVED")}</p>
 
-			<div className="ButtonDiv">
-				<NavLink className="button" to="/involved">
-					{t("GENERAL.GET_INVOLVED")}
-				</NavLink>
+				<div className="ButtonDiv">
+					<NavLink className="button" to="/involved">
+						{t("GENERAL.GET_INVOLVED")}
+					</NavLink>
+				</div>
 			</div>
 		</div>
 	);
