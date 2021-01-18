b8de59032b6d7e566957dbb17199fab7424b60b4
TovahParnes <tovah@parnes.com>
2021-01-18 16:54:17 +0100

    added desctiption to two presentations

--- src/pages/ScheduleFair.jsx
+++ src/pages/ScheduleFair.jsx
@@ -54,11 +54,22 @@ export default function ScheduleFair() {
 
 			<h2>Är du tillräckligt viktig på riktigt?</h2>
 			<h3>11:00-11:40 {t("SCHEDULE.BY")} Pensionsmyndigheten</h3>
+			<p>
+				Cecilia Tiger Fogman och Martina Pulvere berättar om varför Pensionsmyndighetens
+				uppdrag faktiskt berör alla. Både Cecilia och Martina har pluggat teknik på LTU och
+				idag arbetar de på olika sätt med systemutveckling och ledarskap på
+				Pensionsmyndighetens IT-avdelning i Luleå. De delar med sig av sina erfarenheter och
+				svarar på frågor om hur det är att jobba med samhällsviktig IT-utveckling i staten.
+			</p>
 			<p></p>
 
 			<h2>How to have fun at work - and being good at what you do</h2>
 			<h3>12.00-12.45 {t("SCHEDULE.BY")} SKF</h3>
-			<p></p>
+			<p>
+				The SKF reprecentative Dr Victoria Van Camp will answer the questions "What does one
+				have to think about as a newcomer in the work life?", "The way to success" and "How
+				to compete as a newcomer with those that are more experienced" in the presentation.
+			</p>
 
 			<h2>Värdet av rutiner och scheman</h2>
 			<h3>14:00-14:20 {t("SCHEDULE.BY")} Lindbäcks bygg</h3>
