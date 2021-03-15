6a7bd200e8322f608213441d827273ce48c56c01
Tovah Parnes <tovah@parnes.com>
2021-03-15 12:31:45 +0100

    removed unused import

--- src/pages/PG.jsx
+++ src/pages/PG.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { PGApplication } from "../config";
 import { useTranslation } from "react-i18next";
