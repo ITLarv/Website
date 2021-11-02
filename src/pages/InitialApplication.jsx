e7db2b008044f56c38ff334901b2478931013a4f
leo <leohem-0@student.ltu.se>
2021-11-02 08:57:01 +0100

    helptext to IA

--- src/pages/InitialApplication.jsx
+++ src/pages/InitialApplication.jsx
@@ -23,6 +23,12 @@ export default () => {
 				style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}
 			></div>
 			<div id="app-dialog" className="jexpo jexpo-forms"></div>
+
+			<p>
+				Händer det inget efter att du klickat "skicka in"? Kolla din inkorg för
+				bekräftelsemail. Detta är ett tecken på att vi mottagit er anmälan. Hör av er annars
+				till anmalan@larv.org för att stämma av eller för hjälp med er anmälan!
+			</p>
 		</div>
 	);
 };
